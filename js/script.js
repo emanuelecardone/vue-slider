e9d85dd62c28486f75716b56fcb3bc79a8214b8f
emanuelecardone <e_m.cardone@yahoo.it>
2021-12-03 13:57:03 +0100

    Rifatto il layout di base statico con Vue

--- js/script.js
+++ js/script.js
@@ -9,26 +9,31 @@ const app = new Vue(
             sliderObjects: [
                 {
                     image: 'img/01.jpg',
+                    alt: 'Immagine della Svezia',
                     title: 'Svezia',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
                 },
                 {
                     image: 'img/02.jpg',
+                    alt: 'Immagine della Svizzera',
                     title: 'Svizzera',
                     text: 'Lorem ipsum'
                 },
                 {
                     image: 'img/03.jpg',
+                    alt: 'Immagine della Gran Bretagna',
                     title: 'Gran Bretagna',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
                 },
                 {
                     image: 'img/04.jpg',
+                    alt: 'Immagine della Germania',
                     title: 'Germania',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
                 },
                 {
                     image: 'img/05.jpg',
+                    alt: 'Immagine di un\'isola',
                     title: 'Paradise',
                     text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
                 },
